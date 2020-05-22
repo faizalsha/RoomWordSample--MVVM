1685bc8aeecb7d2a8835462d520a2aa7ea889858
shada <shadabfaizal7@gmail.com>
2020-05-22 17:06:51 +0530

    gradle file entry

---
+++ app/src/main/java/com/faizal/shadab/roomwordsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.faizal.shadab.roomwordsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
